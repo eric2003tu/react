cab5bf5b4acaf349924aa6977e92f1ea9cef248b
eric2003tu <mukarugambwaelevanie@gmail.com>
2025-03-11 14:27:26 +0200

    dashboard added

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,6 +1,9 @@
 import React, { useState,useEffect } from 'react';
+import { AiOutlineLike ,AiOutlineDislike} from "react-icons/ai";
 function Posts() {
     const [posts, setPosts] = useState([]);
+    const [like, setLike] = useState(false);
+    const [unlike, setUnlike] = useState(false)
   
     useEffect(() => {
       fetch('https://jsonplaceholder.typicode.com/posts')
@@ -11,14 +14,20 @@ function Posts() {
     return (
       < div>
       <div className='mt-[100px]'>
-        <h1>Posts</h1>
+        <h1 className='font-bold text-[20px]'>Posts</h1>
         <ul className='grid grid-cols-1 justify-self-start justify-start'>
           {posts.map(post => (
-            <li key={post.id}>
-              <h2>{post.title}</h2>
-              <p>{post.body}</p>          
-              <button type='button' className='like-button' onClick={function(){
-            }}>Like</button>
+            <li key={post.id} className='border-1 border-gray-500 p-[10px] mb-[15px] rounded-[5px] justify-items-start'>
+              <h2 className='font-bold text-blue-600'>{post.title}</h2>
+              <p className='justify-self-start'>{post.body}</p>          
+              <button type='button' onClick={function(){
+            }} className="flex items-center space-x-3"><AiOutlineLike size={30} onClick={function(){ 
+              setLike(true)
+              setUnlike(false)
+            }} className={like ===true ? 'cursor-pointer text-green-600 ' : 'text-black cursor-pointer'}/><AiOutlineDislike size={30} onClick ={function(){
+              setUnlike(true)
+              setLike(false)
+            }} className={unlike === true ? 'cursor-pointer text-red-500':' cursor-pointer text-black'}/></button>
             </li>
           ))}
         </ul>
